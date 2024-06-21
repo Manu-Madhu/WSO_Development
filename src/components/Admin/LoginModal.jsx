1e873a481f63fa66cf2596d6e62f96e0e22d7eda
NivedGanga <nivedganga@gmail.com>
2024-06-21 16:42:53 +0530

    placeholder added to login fields

--- src/components/Admin/LoginModal.jsx
+++ src/components/Admin/LoginModal.jsx
@@ -2,10 +2,10 @@ function LoginModal() {
     return (
         <div className="fixed w-[375px] bg-white right-6 p-12 rounded-lg flex flex-col text-black text-sm items-start">
             <label htmlFor="" className="mb-2">Username</label>
-            <input type="text" className="mb-4 p-2 border border-gray-500 rounded w-full" />
+            <input type="text" className="mb-4 p-2 border border-gray-500 rounded w-full" placeholder="Username" />
 
             <label htmlFor="password" className="mb-2">Password</label>
-            <input type="password" className="mb-4 p-2 border border-gray-500 rounded w-full" />
+            <input type="password" className="mb-4 p-2 border border-gray-500 rounded w-full" placeholder="Password" />
 
             <button className="w-full bg-primary-green text-white rounded-md py-3">
                 Sign in
