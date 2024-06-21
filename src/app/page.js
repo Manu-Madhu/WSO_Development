0c17bfa45c72299a323888fd9f9a295b07679fc9
Manu-Madhu <manum66466@gmail.com>
2024-06-21 17:30:02 +0530

    test push

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
        <div>
-         <h1>halo sugamano</h1>
+         <h1>test</h1>
        </div>
     </main>
   );
