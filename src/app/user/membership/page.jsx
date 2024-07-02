a65c77f732b0c02a3558d1c4d9b90d242f80b99d
NivedGanga <nivedganga@gmail.com>
2024-07-02 22:13:41 +0530

    responsiveness updated

--- src/app/user/membership/page.jsx
+++ src/app/user/membership/page.jsx
@@ -9,10 +9,10 @@ const images = [image1, image2, image4, image3];
 function Page() {
     return (
         <div className='w-10/12 mx-auto'>
-            <h1 className="text-title font-bold my-9">Member Registration Form</h1>
+            <h1 className="text-title max-sm:text-2xl font-bold my-9 max-sm:my-5">Member Registration Form</h1>
             <div className='flex h-fit'>
                 <MembershipForm />
-                <div className='flex flex-col gap-12 h-full justify-between'>
+                <div className='flex flex-col gap-12 h-full justify-between max-md:hidden'>
                     {
                         images.map((image, index) => (
                             <Image key={index} src={image} alt="Membership" />
--- src/components/User/about/AboutContent.jsx
+++ src/components/User/about/AboutContent.jsx
@@ -3,8 +3,8 @@ import MemberCard from "./MemberCard";
 function AboutContent({ currentIndex, setCurrentIndex }) {
 
     return (
-        <div className="w-full flex  mt-10">
-            <div className="w-4/12">
+        <div className="w-full flex max-lg:flex-col  mt-10">
+            <div className="w-4/12 max-sm:w-10/12 max-lg:w-full max-md:mx-auto">
                 {
                     about_us.map((item, index) => (
                         <div key={index} className={`cursor-pointer ${currentIndex === index ? 'text-primaryColor font-semibold' : 'text-gray-900 font-medium'} mb-2 `} onClick={() => setCurrentIndex(index)}>
@@ -13,7 +13,7 @@ function AboutContent({ currentIndex, setCurrentIndex }) {
                     ))
                 }
             </div>
-            <div className="w-8/12">
+            <div className="w-8/12 max-sm:w-10/12 max-lg:w-full max-md:mx-auto max-lg:mt-10">
                 {
                     about_us[currentIndex].content.map((item, index) => {
                         if (Array.isArray(item)) {
@@ -27,7 +27,7 @@ function AboutContent({ currentIndex, setCurrentIndex }) {
                         }
                         if (typeof item === 'object') {
                             return (
-                                <div key={index} className="grid grid-cols-4 gap-x-4 gap-y-5">
+                                <div key={index} className="grid grid-cols-4 max-sm:grid-cols-1 max-md:grid-cols-2 gap-x-4 gap-y-5">
                                     {item.members.map((member, subIndex) => (
                                         <MemberCard key={subIndex} {...member} />
                                     ))}
