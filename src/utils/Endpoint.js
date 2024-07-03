d9d205fcae57b9275fe031ff73d23721ca6813b5
knoskimo <abhijithrb91@gmail.com>
2024-07-03 21:21:33 +0530

    new endpoints added

--- src/utils/Endpoint.js
+++ src/utils/Endpoint.js
@@ -1,9 +1,9 @@
 export const baseUrl = 'http://localhost:8800'
 
-export const contactRoute = '/api/contact'
 export const login = '/api/auth/admin/login'
 
 // Guest Routes
+export const contactRoute = '/api/contact'
 
 export const registerRoute = "/api/register";
 
@@ -20,3 +20,8 @@ export const memberNewsletterRoute = "/api/member/newsletter";
 // Admin Routes
 
 export const adminNewsRoute = "/api/admin/news";
+export const adminEventRoute = "/api/admin/event";
+export const adminPublicationRoute = "/api/admin/publication";
+export const adminNewsletterRoute = "/api/admin/newsletter";
+export const adminApplicationRoute = "/api/admin/application";
+export const adminMemberRoute = "/api/admin/member";
