c7553b82935207e08b78ba3981c7810a3ec5202f
NivedGanga <nivedganga@gmail.com>
2024-07-04 21:41:05 +0530

    news api connected in the home page

--- src/components/User/home/about/LatestNews.jsx
+++ src/components/User/home/about/LatestNews.jsx
@@ -1,10 +1,25 @@
-import { News } from "@/data/about";
 
 import Link from "next/link";
 import React from "react";
 import NewsComponent from "./News";
+import { baseUrl, getAllNews } from "@/utils/Endpoint";
 
-const LatestNews = () => {
+async function getData() {
+  try {
+    const res = await fetch(`${baseUrl}${getAllNews}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const data = await res.json()
+    return data.news
+  } catch (error) {
+    toast.error("Something went wrong")
+  }
+}
+const LatestNews = async () => {
+  const News = await getData()
   return (
     <div className="max-w-[1280px] mx-auto w-full px-3 ">
       <h1 className="my-5 md:my-8 text-3xl font-[500]">Latest Updates</h1>
