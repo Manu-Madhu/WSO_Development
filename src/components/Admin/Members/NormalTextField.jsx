ba7de9d714f6af6561a5e9c2e91543d9972ac948
knoskimo <abhijithrb91@gmail.com>
2024-07-04 19:55:08 +0530

    application view

--- src/components/Admin/Members/NormalTextField.jsx
+++ src/components/Admin/Members/NormalTextField.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function NormalTextField({ type, label, placeholder }) {
+function NormalTextField({ type, label, placeholder, value, disabled }) {
     return (
         <div className='flex max-md:flex-col border-t w-10/12 max-md:w-full border-gray-3 00 py-5 justify-between'>
             <div className='w-[35%] max-md:w-full font-medium'>
@@ -9,13 +9,16 @@ function NormalTextField({ type, label, placeholder }) {
             <div className='w-[64%] max-md:w-full'>
                 {type === 'textarea' ? (
                     <textarea
+                        value={value}
+                        disabled={disabled}
                         className="w-full border border-gray-400 mt-1 px-4 py-3 rounded-lg placeholder:text-gray-400 placeholder:font-light"
                         placeholder={placeholder}
                         rows="4"
                     />
                 ) : (
                     <input
-                        type={type}
+                        value={value}
+                        disabled={disabled}
                         className="w-full border border-gray-400 mt-1 px-4 py-3 rounded-lg placeholder:text-gray-400 placeholder:font-light"
                         placeholder={placeholder}
                     />
