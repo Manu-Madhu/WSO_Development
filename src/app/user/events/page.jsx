e6221fb766d0109b4c0398e9fdb112b933a13ca8
Manu M <manum66466@gmail.com>
2024-07-19 02:13:47 +0530

    Update the correct build

--- src/app/user/events/page.jsx
+++ src/app/user/events/page.jsx
@@ -1,41 +1,40 @@
-import EventCard from '@/components/User/events/EventCard'
-import { baseUrl, getAllEvents } from '@/utils/Endpoint'
+import EventCard from "@/components/User/events/EventCard";
+import { baseUrl, getAllEvents } from "@/utils/Endpoint";
 
 async function getData() {
-    try {
-        const res = await fetch(`${baseUrl}${getAllEvents}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        const data = await res.json()
-        return data?.event
-    } catch (error) {
-        console.log("error" + error)
-    }
+  try {
+    const res = await fetch(`${baseUrl}${getAllEvents}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    return data?.event;
+  } catch (error) {
+    console.log("error" + error);
+  }
 }
-
 
 const Events = async () => {
-    const data = await getData()
+  const data = await getData();
 
+  return (
+    <div
+      className="max-w-[1280px] mx-auto w-full p-3 flex flex-col items-center justify-center 
+    my-5 mb-10 md:mb-20 gap-8"
+    >
+      <h1 className=" text-[#101828] font-bold text-2xl self-start ">
+        Events and Meetings
+      </h1>
 
-    return (
-        <div className="max-w-[1280px] mx-auto w-full p-3 flex flex-col items-center justify-center 
-    my-5 mb-10 md:mb-20 gap-8">
-        <h1 className=" text-[#101828] font-bold text-2xl self-start ">Events and Meetings</h1>
+      <div className="w-full flex flex-wrap  gap-4 ">
+        {data?.map((item, i) => (
+          <EventCard key={i} data={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-            <div className='w-full flex flex-wrap  gap-4 '>
-                {
-                    data?.map((item, i) => (
-                        <EventCard key={i} data={item} />
-                    ))
-                }
-            </div>
-
-        </div>
-    )
-}
-
-export default Events
+export default Events;
--- src/components/User/events/EventCard.jsx
+++ src/components/User/events/EventCard.jsx
@@ -1,42 +1,43 @@
-import Image from 'next/image'
-import React from 'react'
-import cardpic from '../../../../public/Assets/user/events/cardpic.png';
+import Image from "next/image";
+import React from "react";
+import cardpic from "../../../../public/Assets/user/events/cardpic.png";
 import { HiOutlineDownload } from "react-icons/hi";
-import Link from 'next/link';
+import Link from "next/link";
 
 const EventCard = ({ data }) => {
+  return (
+    <div className="w-full sm:w-[300px] h-[260px] flex flex-col rounded-lg overflow-hidden relative">
+      {data?.thumbnail ? (
+        <img
+          src={data?.thumbnail?.location}
+          alt="thumbnail"
+          className="w-full h-[180px] object-cover"
+        />
+      ) : (
+        <Image
+          src={cardpic}
+          alt="cards"
+          className="rounded-t-xl w-full h-[70%] object-cover"
+        />
+      )}
 
-    return (
-        <div className='w-full sm:w-[300px] h-[260px] flex flex-col rounded-lg overflow-hidden relative'>
-
-
-            {
-                data?.thumbnail
-                    ?
-                    <img src={data?.thumbnail?.location} alt='thumbnail'
-                        className='w-full h-[180px] object-cover'
-                    />
-                    :
-                    <Image src={cardpic} alt="cards" className="rounded-t-xl w-full h-[70%] object-cover" />
-            }
-
-            <div className='w-full h-[80px] bg-primaryColor text-white flex flex-col justify-center px-3 gap-2' >
-                <div className='w-full flex justify-between items-center'>
-                    <span className='truncate'>{data?.title}</span>
-                    <span className='text-sm font-light'>{data?.date}</span>
-                </div>
-
-                <p className='text-sm font-light truncate'>
-                    {data?.description}
-                </p>
-
-            </div>
-
-            <Link href={data?.document.location ?? ''} className='absolute top-4 right-4 cursor-pointer'>
-                <HiOutlineDownload size={32} color='#777' />
-            </Link>
+      <div className="w-full h-[80px] bg-primaryColor text-white flex flex-col justify-center px-3 gap-2">
+        <div className="w-full flex justify-between items-center">
+          <span className="truncate">{data?.title}</span>
+          <span className="text-sm font-light">{data?.date}</span>
         </div>
-    )
-}
 
-export default EventCard
+        <p className="text-sm font-light truncate">{data?.description}</p>
+      </div>
+
+      <Link
+        href={data?.document?.location ?? ""}
+        className="absolute top-4 right-4 cursor-pointer"
+      >
+        <HiOutlineDownload size={32} color="#777" />
+      </Link>
+    </div>
+  );
+};
+
+export default EventCard;
