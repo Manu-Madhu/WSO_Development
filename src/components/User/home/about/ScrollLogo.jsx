a27867be5c010201a1e05bb5fdbb3da8e545bf9f
Manu-Madhu <manum66466@gmail.com>
2024-10-30 13:07:35 +0530

    test

--- src/components/User/home/about/ScrollLogo.jsx
+++ src/components/User/home/about/ScrollLogo.jsx
@@ -8,7 +8,7 @@ const ScrollLogo = () => {
   return (
     <div className="w-full overflow-hidden mt-10">
       <h1 className="mb-5 lg:mb-10  text-2xl font-[400]">Our Members</h1>
-      <div className="flex flex-row">
+      <div className="flex flex-row overflow-x-scroll my-custom-scrollbar">
         <Marquee velocity={20} resetAfterTries={100} direction="left">
           {/* Wrap logos in a container for spacing */}
           {Logos.map((items) => (
