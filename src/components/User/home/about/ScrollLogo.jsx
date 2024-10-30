4d0d0f38bf198dedb0e99f3aae1e790c1343798c
Manu-Madhu <manum66466@gmail.com>
2024-10-30 13:06:47 +0530

    Update

--- src/components/User/home/about/ScrollLogo.jsx
+++ src/components/User/home/about/ScrollLogo.jsx
@@ -8,11 +8,11 @@ const ScrollLogo = () => {
   return (
     <div className="w-full overflow-hidden mt-10">
       <h1 className="mb-5 lg:mb-10  text-2xl font-[400]">Our Members</h1>
-      <div>
+      <div className="flex flex-row">
         <Marquee velocity={20} resetAfterTries={100} direction="left">
           {/* Wrap logos in a container for spacing */}
           {Logos.map((items) => (
-            <div key={items.id} className="flex items-center justify-center mx-5 lg:mx-12 mb-5 lg:mb-0 "> {/* Add margin for spacing */}
+            <div key={items.id} className="flex items-center justify-center mx-2 lg:mx-12 mb-5 lg:mb-0 "> {/* Add margin for spacing */}
               <Image
                 src={items.logo}
                 alt="logo"
